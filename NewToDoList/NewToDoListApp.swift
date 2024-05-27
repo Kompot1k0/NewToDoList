d45869e1d8ae3313b2734462ce0d39d6b7f25de7
Eduard Marrugo <marruguera@gmail.com>
2024-05-27 12:28:45 +0300

    Initial Commit

---
+++ NewToDoList/NewToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewToDoListApp.swift
+//  NewToDoList
+//
+//  Created by Admin on 27.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
